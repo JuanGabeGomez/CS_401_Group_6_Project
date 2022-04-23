85b2a4641e716f4908472d9cadf73b3cf874b422
JohnParkerWilson <98854251+JohnParkerWilson@users.noreply.github.com>
2022-04-22 22:53:09 -0700

    Update Customer.java
    changed variables
    added readCustomerInfo function to read the toString output stuff
    added addAccounts function

--- Customer.java
+++ Customer.java
@@ -1,41 +1,109 @@
+import java.util.*;
 
 public class Customer {
-	String name;
+	String lastName;
+	String firstName;
 	String address;
 	String customerID;
+	String card;
+	String pin;
 	String [] accounts;
+	int accountsSize;
 	
 	public Customer () {
-		name = "Jeff";
+		firstName = "Jeff";
+		lastName = "Jefferson";
+		pin = "0000";
+		card = "0000";
 		address = "1234 Potato Street";
-		customerID = "";
+		customerID = "0000";
 		accounts = new String[10];
+		accountsSize = 0;
 	}
 	
-	public Customer(String newName,String newAddress,String newID,String [] newAccounts) {
-		name = newName;
+	public Customer(String newFirstName, String newLastName,String newAddress,String newID,
+			String newPin, String [] newAccounts) {
+		firstName = newFirstName;
+		lastName = newLastName;
 		address = newAddress;
 		customerID = newID;
+		pin = newPin;
 		accounts = newAccounts;
+		accountsSize = newAccounts.length;
 	}
 	
 	public String toString() {
 		String customerinfo = "";
 		
 		customerinfo = customerID + " , "
-				+ name + " , "
-				+ address + " , ";
+				+ firstName + " , "
+				+ lastName + " , "
+				+ address + " , "
+				+ card + " , "
+				+ pin + " , ";
 		
-		for(int i = 0;i<accounts.length;i++) {
+		for(int i = 0;i<accountsSize;i++) {
 			customerinfo += (accounts[i] + " ");
 		}
+		customerinfo += " @ ";
 		
 		return customerinfo;
 	}
 	
+	public void readString() {
+		
+	}
 	
-	public String getName() {
-		return name;
+	public void readCustomerInfo(String customerInfo) {
+		Scanner sc = new Scanner(customerInfo);
+		accountsSize = 0;
+
+		String buffer;
+		//id
+		buffer = sc.next();
+		customerID = buffer;
+		buffer = sc.next();
+		//first name
+		buffer = sc.next();
+		firstName = buffer;
+		buffer = sc.next();
+		//last name
+		buffer = sc.next();
+		lastName = buffer;
+		buffer = sc.next();
+		buffer = sc.next();
+		// address
+		address = "";
+		while (!buffer.contentEquals(",")) {
+			address += (buffer + " ");
+			buffer = sc.next();
+		}
+		// card
+		buffer = sc.next();
+		card = buffer;
+		buffer = sc.next();
+		// pin
+		buffer = sc.next();
+		pin = buffer;
+		buffer = sc.next();
+		buffer = sc.next();
+		//accounts
+		while (!buffer.contentEquals("@")) {
+			addAccount(buffer);
+			buffer = sc.next();
+		}
+	}
+	
+	public void addAccount(String newAccount) {
+		accounts[accountsSize] = newAccount;
+		accountsSize++;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
 	}
 	public String getAddress() {
 		return address;
@@ -47,8 +115,11 @@ public class Customer {
 		return accounts;
 	}
 	
-	public void setName(String x) {
-		name = x;
+	public void setFirstName(String x) {
+		firstName = x;
+	}
+	public void setLastName(String x) {
+		firstName = x;
 	}
 	public void setAddress(String x) {
 		address = x;
